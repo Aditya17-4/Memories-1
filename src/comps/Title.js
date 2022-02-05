b831528e65a4e3cf9e3dbf2869ae3f5dd749feaf
Aditya17-4 <58966618+Aditya17-4@users.noreply.github.com>
2022-02-05 18:08:54 +0530

    second commit

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
-  return (
-    <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </div>
-  )
-}
+	return (
+		<div className="title">
+			<h1>Memories</h1>
+			<h2>Our Pictures</h2>
+			<p>
+				A picturewall dedicated to ourselfs to store the best of our memories.
+			</p>
+		</div>
+	);
+};
 
 export default Title;
